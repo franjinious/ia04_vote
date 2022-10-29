d8e957fad57ae32c4f83b0a58cc8958926191000
whz <122885887@qq.com>
2022-10-29 23:03:54 +0200

    update file voteragent.go

--- agt/voteragent/voteragent.go
+++ agt/voteragent/voteragent.go
@@ -1,1 +1,23 @@
 package voteragent
+
+//
+// agent pour voter
+//
+
+type Voteragent struct {
+
+}
+
+const (
+	// for api /vote
+	VoteSuccess = 200
+	BadRequest = 400
+	UselessVote = 403
+	NotImplemented = 501
+	TimeOut = 503
+
+	// for api /result
+	OK = 200
+	TooEarly = 425
+	Notfind = 404
+)
